f0133256ffc3da09cb6694d9d32f8d491cb82419
gadhagod <69025547+gadhagod@users.noreply.github.com>
2020-11-09 18:40:21 -0800

    Fix stuff

--- static/redirect.js
+++ static/redirect.js
@@ -1,4 +1,5 @@
 function search(){
     var channel = document.getElementById('query').value;
+    document.write('loading...')
     window.location.href = 'https://gif-invasion.herokuapp.com//search/' + channel;
 }
